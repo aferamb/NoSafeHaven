6dff8dc8be569e7531ef5882c6d4dc4659938b37
alexpg2019 <05jandro@gmail.com>
2025-04-03 12:12:15 +0200

    Initialize NoSafeHaven project with Maven configuration

---
+++ NoSafeHaven/src/main/java/apocalipsis/nosafehaven/NoSafeHaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package apocalipsis.nosafehaven;
+
+/**
+ *
+ * @author 05jan
+ */
+public class NoSafeHaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
