5afd14c6dd85c4b06149a114e652385366064bbb
Alejandro <84287430+aferamb@users.noreply.github.com>
2025-04-26 19:33:15 +0200

    main basico creado, no probado

--- NoSafeHaven/src/main/java/apocalipsis/nosafehaven/NoSafeHaven.java
+++ NoSafeHaven/src/main/java/apocalipsis/nosafehaven/NoSafeHaven.java
@@ -11,6 +11,24 @@ package apocalipsis.nosafehaven;
 public class NoSafeHaven {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Crear el Refugio y el Exterior
+        Refugio refugio = new Refugio();
+        Exterior exterior = new Exterior();
+        
+        // Crear un número de Humanos
+        int numHumanos = 10; // Puedes cambiar el número que quieras
+        for (int i = 0; i < numHumanos; i++) {
+            Humano humano = new Humano("Humano-" + i, refugio, exterior);
+            humano.start(); // Arrancar cada hilo de Humano
+            try {
+                Thread.sleep((int) (Math.random() * 500 + 1500)); //en zona comun 1 a 2 seg
+            } catch (Exception e) {
+            }
+        }
+
+        // Nota: Los Zombis se generarán automáticamente cuando un humano muera
+        // por lo tanto aquí no creamos zombies manualmente.
+
+        // El programa no termina nunca porque los humanos y zombis son hilos que funcionan "para siempre" (hasta morir o eternamente en el caso de los zombis).
     }
 }
